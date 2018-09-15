a37eafdd8cfd7c2c2022318f70c68d152a1fa987
Shalom Rachapudi David <shalomray7@linux-rjee.suse>
2018-09-15 18:23:55 +0530

    Restructure directory layout

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 # uncomment the following line to run tests
 # go test
+cd ./code
 go run main.go crawler.go html_parser.go misc.go downloader.go -c 20 -url "https://www.amazon.co.uk/" | tee output.out
